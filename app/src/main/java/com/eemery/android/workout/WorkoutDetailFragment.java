0c1a1b3442039b5d2f69a009904ef1a4cc04d83b
Eric Emery <eric.r.emery@gmail.com>
2018-10-10 16:41:14 -0500

    feat: Added timer to each workout per Chapter 11 of Head First Android Development

--- app/src/main/java/com/eemery/android/workout/WorkoutDetailFragment.java
+++ app/src/main/java/com/eemery/android/workout/WorkoutDetailFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,7 +23,14 @@ public class WorkoutDetailFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (savedInstanceState != null) {
+        if (savedInstanceState == null) {
+            StopwatchFragment stopwatchFragment= new StopwatchFragment();
+            FragmentTransaction fragmentTransaction = getChildFragmentManager().beginTransaction();
+            fragmentTransaction.add(R.id.stopwatch_container, stopwatchFragment);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            fragmentTransaction.commit();
+        } else {
             workoutId = savedInstanceState.getLong("workoutId");
         }
     }
